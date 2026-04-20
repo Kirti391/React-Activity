1c7a07b1d6d35de0bd4cbe534db17e5068f16ece
Kirti <162712321+Kirti391@users.noreply.github.com>
2026-04-20 19:41:17 +0530

    Initial Commit

---
+++ React-Activity2/Amazon Cards/src/Price.jsx
@@ -0,0 +1,9 @@
+ function Price({oldPrice,newPrice}){
+    return (
+        <div>
+        <span>{oldPrice}</span>
+        <span>{newPrice}</span>
+        </div>
+    )
+}
+export default Price;
---
+++ React-Activity2/Amazon Cards/src/ProductTab.jsx
@@ -0,0 +1,13 @@
+import Product from "./Product .jsx"
+
+export default function ProductTab(){
+    return(
+        <div>
+         <Product title="Logitech MXMaster" idx={0}/>
+          <Product title="Apple Pencil (2nd Gen)"idx={1}/>
+           <Product title="Zebronics Zeb Transformer"idx={2}/>
+           <Product title="Petronics Toad 23" idx={3}/>
+
+        </div>
+    )
+}
